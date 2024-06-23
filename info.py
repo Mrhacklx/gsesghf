ef9a110375e20b8bec5c1f3482bc1ec552251e60
Mrhacklx <142010590+Mrhacklx@users.noreply.github.com>
2024-06-23 11:46:03 +0530

    Update info.py

--- info.py
+++ info.py
@@ -14,8 +14,8 @@ def is_enabled(value, default):
 
 # Bot information
 SESSION = environ.get('SESSION', 'Mrhacklx')
-API_ID = int(environ.get('API_ID', '22570090'))
-API_HASH = environ.get('API_HASH', '6f247cd7dfa87c0e6a2ee2931b52eb0b')
+API_ID = int(environ.get('API_ID', '27382550'))
+API_HASH = environ.get('API_HASH', 'c428034cb811de290315501d8e3c82b5')
 BOT_TOKEN = environ.get('BOT_TOKEN', "")
 
 # Bot settings
@@ -27,7 +27,7 @@ SPELL_IMG = environ.get("SPELL_IMG", "https://te.legra.ph/file/15c1ad448dfe472a5
 
 # Admins, Channels & Users
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002142649224'))
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '2102808330').split()]
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '6739173851').split()]
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002013490678').split()]
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
