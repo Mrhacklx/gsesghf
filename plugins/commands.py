ba02d227a46edab20c67a1ded8d78b239db0debd
Mrhacklx <142010590+Mrhacklx@users.noreply.github.com>
2024-10-06 16:34:54 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -368,7 +368,7 @@ async def start(client, message):
     if data.startswith("sendfiles"):
         chat_id = int("-" + file_id.split("-")[1])
         userid = message.from_user.id if message.from_user else None
-        g = f"https://kvl.bisgram.com/{temp.U_NAME}?start=allfiles_{file_id}"
+        g = f"https://open1o.blogspot.com/{temp.U_NAME}?start=allfiles_{file_id}"
         k = await client.send_message(chat_id=message.from_user.id,text=f"<b>Get All Files in a Single Click!!!\n\n📂 ʟɪɴᴋ ➠ : {g}\n\n<i>Note: This message is deleted in 5 mins to avoid copyrights. Save the link to Somewhere else</i></b>", reply_markup=InlineKeyboardMarkup(
                 [
                    [
@@ -387,7 +387,7 @@ async def start(client, message):
         chat_id = temp.SHORT.get(user)
         files_ = await get_file_details(file_id)
         files = files_[0]
-        g = f"https://kvl.bisgram.com/{temp.U_NAME}?start=file_{file_id}"
+        g = f"https://open1o.blogspot.com/{temp.U_NAME}?start=file_{file_id}"
         ads = "https://www.highcpmgate.com/wqbgbib04c?key=5a2a56217484360019d0067613c620d2"
         k = await client.send_message(chat_id=user,text=f"<b>📕Nᴀᴍᴇ ➠ : <code>{files.file_name}</code> \n\n🔗Sɪᴢᴇ ➠ : {get_size(files.file_size)}\n\n📂Fɪʟᴇ ʟɪɴᴋ ➠ : {g}\n\n<i>Note: This message is deleted in 20 mins to avoid copyrights. Save the link to Somewhere else</i></b>", reply_markup=InlineKeyboardMarkup(
                 [
@@ -465,7 +465,7 @@ async def start(client, message):
         if settings['is_shortlink'] and user not in PREMIUM_USER:
             files_ = await get_file_details(file_id)
             files = files_[0]
-            g = f"https://kvl.bisgram.com/{temp.U_NAME}?start=file_{file_id}"
+            g = f"https://open1o.blogspot.com/{temp.U_NAME}?start=file_{file_id}"
             k = await client.send_message(chat_id=message.from_user.id,text=f"<b>📕Nᴀᴍᴇ ➠ : <code>{files.file_name}</code> \n\n🔗Sɪᴢᴇ ➠ : {get_size(files.file_size)}\n\n📂Fɪʟᴇ ʟɪɴᴋ ➠ : {g}\n\n<i>Note: This message is deleted in 20 mins to avoid copyrights. Save the link to Somewhere else</i></b>", reply_markup=InlineKeyboardMarkup(
                     [
                          [
